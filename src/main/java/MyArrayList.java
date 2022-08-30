762a8a7d9b0527a40398070f09b4779074d9f319
Yaroslav <syrovatkaaroslav@gmail.com>
2022-08-30 23:16:11 +0300

    MyArray v.5

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -6,11 +6,51 @@ public class MyArrayList<E> {
 
     public void add(E value) {
         if (number == array.length) {
-            array = Arrays.copyOf(array, array.length + array.length/2);
+            array = Arrays.copyOf(array, array.length * 3/2);
         }
         array[number] = value;
         number++;
         System.out.println(Arrays.toString(array));
+    }
+
+    public void remove(int index) {
+        for(int i = 0; i < array.length; i++) {
+            for (int k = 0; k < array.length - 1; k++) {
+                if (i == index) {
+                    Object temp = array[k];
+                    array[k] = array[k + 1];
+                    array[k + 1] = temp;
+                }
+            }
+        }
+        number--;
+        array = Arrays.copyOf(array, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public void clear() {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                array[i] = null;
+            }
+        }
+        number = 0;
+        System.out.println(Arrays.toString(array));
+    }
+
+    public void size() {
+        int counter = 0;
+        for (Object o : array) {
+            if (o != null) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+
+
+    public Object get(int index) {
+        return array[index];
     }
 }
 
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,21 +2,7 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        MyArrayList<Integer> a = new MyArrayList<>();
-        a.add(1);
-        a.add(2);
-        a.add(3);
-        a.add(4);
-        a.add(5);
-        a.add(6);
-        a.add(7);
-        a.add(8);
-        a.add(9);
-        a.add(10);
-        a.add(11);
-        a.add(12);
-        a.add(13);
-        a.add(14);
+
     }
 
 }
