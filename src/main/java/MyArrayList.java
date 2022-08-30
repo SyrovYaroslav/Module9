5ab24462eb23ff76fc02a9347a7ff2ea0dd24295
Yaroslav <syrovatkaaroslav@gmail.com>
2022-08-30 14:48:56 +0300

    MyArray v.4

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -5,28 +5,12 @@ public class MyArrayList<E> {
     private int number = 0;
 
     public void add(E value) {
-        array[number] = value;
-        number++;
         if (number == array.length) {
             array = Arrays.copyOf(array, array.length + array.length/2);
         }
+        array[number] = value;
+        number++;
         System.out.println(Arrays.toString(array));
-    }
-
-    public Object[] getArray() {
-        return array;
-    }
-
-    public void setArray(Object[] array) {
-        this.array = array;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
     }
 }
 
